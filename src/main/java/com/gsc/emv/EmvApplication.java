5ff525dc3eeff92dabe84b2ea39b6639901c712a
luisrigor <luis.parra@rigorcg.pt>
2023-05-18 19:56:35 -0500

    new project emv

---
+++ src/main/java/com/gsc/emv/EmvApplication.java
@@ -0,0 +1,13 @@
+package com.gsc.emv;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EmvApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EmvApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/gsc/emv/EmvApplicationTests.java
@@ -0,0 +1,13 @@
+package com.gsc.emv;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EmvApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}

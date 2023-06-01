7c4eea11cdd1bed5efcabcc1a78d5d6749448cd1
RonaldGSC <ronald.maldonado@parceiro.rigorcg.pt>
2023-06-01 16:20:37 -0500

    FIX: fix annotations for entity

--- src/main/java/com/gsc/portalapps/model/entity/ServiceLogin.java
+++ src/main/java/com/gsc/portalapps/model/entity/ServiceLogin.java
@@ -2,6 +2,9 @@ package com.gsc.portalapps.model.entity;
 
 import lombok.Getter;
 import lombok.Setter;
+import javax.validation.constraints.NotEmpty;
+import javax.validation.constraints.NotNull;
+import javax.validation.constraints.Size;
 import javax.persistence.*;
 import java.io.Serializable;
 import java.time.LocalDateTime;
